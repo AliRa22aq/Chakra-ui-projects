60452b1ec3403438ec0f56d90916da23e1b81693
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2022-10-05 19:09:13 +0500

    Updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,37 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { Box, Heading, Text, Stack} from '@chakra-ui/react'
 import Link from 'next/link'
+
+
+const projects = [
+  {
+    name: "Project 1",
+    url: "project1",
+    image: "/images/1.png" 
+  }
+]
 
 
 const Home: NextPage = () => {
   return (
     <Box textAlign="center">
 
-      <Heading m="5"> Welcome </Heading>
-      
-      <Stack>
-        <Link href="project1">
-          <Text as="h3" cursor="pointer"> Project 1 </Text>
-        </Link>
+      <Heading my="20" > Chakra UI Demo Projects</Heading>
 
+      <Stack direction="row" m="20">
+        {
+          projects.map((project) => (
+            <Box cursor="pointer" boxShadow=" 0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04);">
+              <Link href={project.url}>
+                <Box>
+                  <Image src={project.image} width="500px" height="300px" alt={project.name} />
+                  <Text as="p"> {project.name} </Text>
+                </Box>
+              </Link>
+            </Box>
+          ))
+        }
       </Stack>
 
       
